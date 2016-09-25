0b2fb142da29930dff7df0afab1449da7e378b19
jdr3 <jdr3@61224a88-a156-e611-bf22-b8ac6f268050>
2016-09-25 20:15:46 +0000

    We have shadows!

--- students/2-rays/jdr3+jsg2/source/RayTracer.h
+++ students/2-rays/jdr3+jsg2/source/RayTracer.h
@@ -3,11 +3,16 @@
 
 /** */
 class RayTracer {
+    protected:
+         shared_ptr<TriTree> m_triangles;
     public:
-        Radiance3 measureLight(const shared_ptr<Scene>& scene, const Ray ray, int numScatters, const TriTree& triangles, const CPUVertexArray& vertices);
-        bool findIntersection(shared_ptr<Surfel>& surfel, const Ray ray, const TriTree& triangles, const CPUVertexArray& vertices);
+        RayTracer();
+        ~RayTracer();
+        Radiance3 measureLight(const shared_ptr<Scene>& scene, const Ray ray, int numScatters);
+        bool findIntersection(shared_ptr<Surfel>& surfel, const Ray ray);
         bool findSphereIntersection(const shared_ptr<Surfel>& surfel, const Ray ray, const Point3 center, const float radius);
         bool findTriangleIntersection(const Ray ray, const Tri triangle, const CPUVertexArray& vertices, float& t, float b[3], TriTree::Hit hit);
         void rayTrace(const shared_ptr<Scene>& scene, const shared_ptr<Camera>& cam, const shared_ptr<Image>& image);
         Radiance3 shade(const Ray ray, const shared_ptr<Surfel>& surfel, const Array<shared_ptr<Light>>& lights);
+        bool isVisible(Point3 X, Point3 Y);
 };
